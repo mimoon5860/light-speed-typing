3c37c37e12c2e92506599fea193a406abc09e12a
mimoon5860 <mahmodulislammoon@gmail.com>
2021-10-04 08:41:20 +0800

    about contact page added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import img from '../../images/about.png'
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
+        <div className='flex items-center'>
+            <div className='w-1/2'>
+                <img className='w-auto' src={img} alt="" />
+            </div>
+            <div className='w-1/2'>
+                <h1 className='text-4xl font-bold'>LightSpeed Typing</h1>
+                <h1 className='text-9xl font-black'>About Us</h1>
+                <div className='text-justify w-3/4'>
+                    <p>LightSpeed Typing is the most effective way to learn how to type. It is web based, and highly effective. LightSpeed Typing is (and will always be) free for both individuals and schools. There is an optional paid school edition.</p>
+                    <hr />
+                    <h1>How Does it Work?</h1>
+                    <p>Keep practicing each lesson until you get all five stars. It really doesn't take much to learn, a few minutes a day for one to two weeks and you will be a pro!</p>
+                    <hr />
+                    <h1>Do I need an account?</h1>
+                    <p>You do not need to create an account. However, as you go through the lessons, you can create an optional profile in order to save your progress.</p>
+                    <hr />
+                    <h1>About us</h1>
+                    <p>We are a team of developers, designers, and content creators based in Rockville, Maryland. Our focus is on creating cutting edge educational platforms designed to enhance your students’ learning experience.</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import Fetch from '../Utilitis/Utilitis';
+import img from '../../images/contact.png'
 
 const ContactUs = () => {
-    const eee = Fetch();
-    console.log(eee)
+
     return (
         <div>
-            <h1>Contact us</h1>
+            <div className='w-1/3 m-4 mx-auto'>
+                <img src={img} alt="" />
+            </div>
+            <div className='text-center m-4'>
+                <h1 className=''>Contact Us</h1>
+                <h3>Reach out us if you have any questions.</h3>
+                <h3>We are here to help you.</h3>
+            </div>
+            <div className='flex justify-around text-center m-4'>
+                <div>
+                    <h1>General enquiries</h1>
+                    <h1>info@lightspeedtyping.com</h1>
+                </div>
+                <div>
+                    <h1>Technical enquiries</h1>
+                    <h1>admin@lightspeedtyping.com</h1>
+                </div>
+            </div>
         </div>
     );
 };
